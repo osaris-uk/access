3179fe825103d0ae8274020f62bca0f789e6e04b
John Dent <john.dent@osaris.co.uk>
2023-06-20 23:42:16 +0100

    Testing

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,15 +2,32 @@
 
 namespace OsarisUk\Access\Tests;
 
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\ServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
 use OsarisUk\Access\AccessServiceProvider;
 
 class TestCase extends Orchestra
 {
-    protected function getPackageProviders($app)
+    use RefreshDatabase;
+
+    /**
+     * @param  Application  $app
+     * @return array<int, class-string<ServiceProvider>>
+     */
+    protected function getPackageProviders($app): array
     {
         return [
             AccessServiceProvider::class,
         ];
+    }
+
+    /**
+     * @return void
+     */
+    protected function defineDatabaseMigrations()
+    {
+        $this->loadLaravelMigrations();
     }
 }
